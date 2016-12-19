6dfb7faeb0108750d7b48cc81f532437e35f9cae
ja <ja>
2016-12-19 14:01:45 +0800

    move cms to other directory

--- Jango.CMS.V1/Jango.CMS/Jango.CMS.Domain/Aggregate/IEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Jango.CMS.Domain.Aggregate
-{
-    public interface IEntity
-    {
-        Guid Id { get; }
-    }
-}
--- Jango.CMS.V1/Jango.CMS/Jango.CMS.Domain/Model/AggregateRoot.cs
+++
@@ -1,9 +0,0 @@
-﻿using Jango.CMS.Domain.Aggregate;
-
-namespace Jango.CMS.Domain.Model
-{
-    public class AggregateRoot:Entity,IAggregateRoot
-    {
-
-    }
-}
